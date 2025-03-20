3499aa4afa37171a883197a1878cc61a2d97c4fa
Lukas Schaab <lukas.schaab@lukaio.de>
2025-03-20 15:27:39 +0100

    added button as first user input

--- app.js
+++ app.js
@@ -1,31 +1,43 @@
-
 const options = {
-    enableHighAccuracy: true,
-    timeout: 5000,
-    maximumAge: 0,
-  };
-  
-  function success(pos) {
-    const crd = pos.coords;
-  
-    document.getElementById("lat").innerHTML = `Latitude : ${crd.latitude}`;
-    document.getElementById("lon").innerHTML = `Longitude: ${crd.longitude}`;
-    document.getElementById("acc").innerHTML = `More or less ${crd.accuracy} meters.`;
-  }
-  
-  function error(err) {
-    console.warn(`ERROR(${err.code}): ${err.message}`);
-  }
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0,
+};
 
-  function handleOrientation(event) {
-    let heading = 360 - event.alpha;
-    
-    document.getElementById("north").innerHTML = `Heading: ${Math.round(heading)}° `;
-    document.getElementById("alpha").innerHTML = `alpha: ${Math.round(event.alpha)}° `;
-    document.getElementById("beta").innerHTML = `beta: ${Math.round(event.beta)}° `;
-    document.getElementById("gamma").innerHTML = `gamma: ${Math.round(event.gamma)}° `;
-  }
+function success(pos) {
+  const crd = pos.coords;
 
-  // Initialize on start
-  navigator.geolocation.getCurrentPosition(success, error, options);
+  document.getElementById("lat").innerHTML = `Latitude : ${crd.latitude}`;
+  document.getElementById("lon").innerHTML = `Longitude: ${crd.longitude}`;
+  document.getElementById(
+    "acc"
+  ).innerHTML = `More or less ${crd.accuracy} meters.`;
+}
+
+function error(err) {
+  console.warn(`ERROR(${err.code}): ${err.message}`);
+}
+
+function handleOrientation(event) {
+  let heading = 360 - event.alpha;
+
+  document.getElementById("north").innerHTML = `Heading: ${Math.round(
+    heading
+  )}° `;
+  document.getElementById("alpha").innerHTML = `alpha: ${Math.round(
+    event.alpha
+  )}° `;
+  document.getElementById("beta").innerHTML = `beta: ${Math.round(
+    event.beta
+  )}° `;
+  document.getElementById("gamma").innerHTML = `gamma: ${Math.round(
+    event.gamma
+  )}° `;
+}
+
+// Initialize on start
+const btn = document.getElementById("start");
+btn.addEventListener("click", () => {
+  navigator.geolocation.watchPosition(success, error, options);
   window.addEventListener("deviceorientation", handleOrientation);
+});
