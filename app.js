13b4e58d54cb726b9d689225e83b48ccd1888303
Lukas Schaab <lukas.schaab@lukaio.de>
2025-03-20 14:37:50 +0100

    get geoData by button

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const btn = document.getElementById("startButton");
+    btn.addEventListener("click", () => {
+        navigator.geolocation.getCurrentPosition(success, error, options);
+    });
+});
+
 const options = {
     enableHighAccuracy: true,
     timeout: 5000,
@@ -7,7 +14,6 @@ const options = {
   function success(pos) {
     const crd = pos.coords;
   
-    console.log("Your current position is:");
     document.getElementById("lat").innerHTML = `Latitude : ${crd.latitude}`;
     document.getElementById("lon").innerHTML = `Longitude: ${crd.longitude}`;
     document.getElementById("acc").innerHTML = `More or less ${crd.accuracy} meters.`;
@@ -16,5 +22,3 @@ const options = {
   function error(err) {
     console.warn(`ERROR(${err.code}): ${err.message}`);
   }
-  
-  navigator.geolocation.getCurrentPosition(success, error, options);
